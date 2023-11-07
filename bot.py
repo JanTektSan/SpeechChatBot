342005ca03640419df5039e8ba0bef394d5c0bbb
jantektsan <yuniverse9999@hotmail.com>
2023-11-07 13:35:31 +0900

    simple speech-to-text and reply by speech

---
+++ bot.py
@@ -0,0 +1,62 @@
+import numpy as np
+import speech_recognition as sr
+from gtts import gTTS
+import os
+import datetime
+
+# Beginning of the AI
+class ChatBot():
+    def __init__(self, name):
+        print("----- starting up", name, "-----")
+        self.name = name
+
+    def speech_to_text(self):
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as mic:
+            print("listening...")
+            audio = recognizer.listen(mic)
+        try:
+            self.text = recognizer.recognize_google(audio)
+            print("me --> ", self.text)
+        except:
+            print("me -->  ERROR")
+    
+    def wake_up(self, text):
+        return True if self.name in text.lower() else False
+
+    @staticmethod
+    def text_to_speech(text):
+        print("AI --> ", text)
+        speaker = gTTS(text=text, lang="en", slow=False)
+        speaker.save("res.mp3")
+        os.system("start res.mp3")  #if you have a macbook->afplay or for windows use->start
+        os.remove("res.mp3")
+    
+    @staticmethod
+    def action_time():
+        return datetime.datetime.now().time().strftime('%H:%M')
+
+# Execute the AI
+if __name__ == "__main__":
+    ai = ChatBot(name="Dev")
+    while True:
+        ai.speech_to_text()
+        ## wake up
+        if ai.wake_up(ai.text) is True:
+            res = "Hello I am Dev the AI, what can I do for you?"
+        ## do any action
+        elif "time" in ai.text:
+            res = ai.action_time()
+        ## respond politely
+        elif any(i in ai.text for i in ["thank","thanks"]):
+            res = np.random.choice(
+                ["you're welcome!","anytime!",
+                "no problem!","cool!",
+                "I'm here if you need me!","peace out!"])
+        ai.text_to_speech(res)
+
+    
+
+
+
+
